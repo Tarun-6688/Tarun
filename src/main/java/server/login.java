94df0feefb54186c70c17f7cf1fd02ad7fb1c8f9
Tarun Kumar <tarunkumarkampasati88@gmail.com>
2025-05-04 11:11:55 +0530

    Update login.java

--- src/main/java/server/login.java
+++ src/main/java/server/login.java
@@ -1,11 +1,6 @@
-package server;
+ package server;
 
-import java.beans.PersistenceDelegate;
 import java.io.IOException;
-import java.net.http.HttpRequest;
-
-import com.sun.xml.internal.stream.Entity;
-
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
@@ -15,27 +10,29 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import jakarta.servlet.RequestDispatcher;
 import object.entity;
 
 @WebServlet("/tarun1")
 public class login extends HttpServlet {
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		int id = Integer.parseInt(req.getParameter("id"));
-		String name = req.getParameter("name");
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("tarun");
-		EntityManager em = emf.createEntityManager();
-		EntityTransaction tr = em.getTransaction();
-		entity e1 = em.find(entity.class, id);
-		if (e1 != null && e1.getName().equals(name)) {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int id = Integer.parseInt(req.getParameter("id"));
+        String name = req.getParameter("name");
 
-			resp.sendRedirect("display.html");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("tarun");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tr = em.getTransaction();
 
-		} else {
-			resp.sendRedirect("register.html");
+        entity e1 = em.find(entity.class, id);
 
-		}
-
-	}
-
+        if (e1 != null && e1.getName().equals(name)) {
+            resp.sendRedirect("display.html");
+        } else {
+            req.setAttribute("errorMessage", "Invalid ID or Password.");
+            RequestDispatcher dispatcher = req.getRequestDispatcher("login.jsp");
+            dispatcher.forward(req, resp);
+        }
+    }
 }
